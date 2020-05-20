2b603fedcbf3b2b270a0386ab6d0644f6f70e47d
Artur Robek <robek.artur@gmial.com>
2020-05-20 13:18:00 +0200

    chenges in includes

--- .history/medjar/index_20200520131525.php
+++ .history/medjar/index_20200520131525.php
@@ -1,5 +1,5 @@
 <?php include('includes/header.php'); ?>
 <title>Meduza Jarosławiec</title>
-<?php include('includes/navbar.php'); ?>
+<?php include('includes/navbar.php')?>
 
 <?php include('includes/footer.php'); ?>
