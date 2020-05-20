3f686e0fe32b4dd017bdffde39e63004296413c7
Artur Robek <robek.artur@gmial.com>
2020-05-20 11:46:08 +0200

    add header and footer

---
+++ .history/medjar/includes/header_20200520114506.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="pl">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
+
+        <!-- Custom Styles-->
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" href="compact-gallery.css">
+
+        <title>Meduza Jarosławiec</title>
+        <link rel = "icon" type = "image/png" href = "">
+    </head>
+    <body>
+        <!-- nav-->
+        <nav class="navbar navbar-expand-lg navbar-dark sticky-top" id="navibar">
+            <a class="navbar-brand " href="#"><alt src="" height="35" width="35" class="d-inline-block"></alt>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-right" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="jaroslawiec.php">Jarosławiec</a>
+                    <a class="nav-item nav-link active" href="noclegi.php">Noclegi</a>
+                    <a class="nav-item nav-link active" href="cennik.php">Cennik</a>
+                    <a class="nav-item nav-link active" href="galeria.php">Galeria</a>
+                    <a class="nav-item nav-link active" href="kontakt.php">Kontakt</a>
+                </div>
+            </div>
+        </nav>
+<!-- end nav-->
+
+<!--Section1-->
+<section id="sec1">
+    <div id="carouselGallery" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselGallery" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselGallery" data-slide-to="1"></li>
+                <li data-target="#carouselGallery" data-slide-to="2"></li>
+            </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="image/" height="600" width="1200" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <p><a role="button" class="btn btn-primary">NOCLEGI JAROSŁAWIEC</a></p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="images/" height="600" width="1200" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <p><a role="button" class="btn btn-primary">NOCLEGI JAROSŁAWIEC</a></p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="images/" height="600" width="1200" class="d-block w-100" alt="..." >
+                <div class="carousel-caption d-none d-md-block">
+                    <p><a role="button" class="btn btn-primary">NOCLEGI JAROSŁAWIEC</a></p>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselGallery" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselGallery" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</section>
+        <!-- end sec1 -->
