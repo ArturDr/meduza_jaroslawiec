7b9c72950137bf6a58710aa0fd73bada5f99fd74
Artur Robek <robek.artur@gmial.com>
2020-05-25 19:13:00 +0200

    add sections in jaroslawiec, changes in index, nav

--- medjar/.history/includes/navbar_20200520131524.php
+++ medjar/.history/includes/navbar_20200520131524.php
@@ -6,7 +6,7 @@
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                     <a class="nav-item nav-link active" href="jaroslawiec.php">Jarosławiec</a>
                     <a class="nav-item nav-link active" href="noclegi.php">Noclegi</a>
---
+++ medjar/.history/jaroslawiec_20200525185218.php
@@ -0,0 +1,21 @@
+<?php include('includes/header.php'); ?>
+
+<title>Jarosławiec | Meduza Jarosławiec</title>
+
+<?php include('includes/navbar.php'); ?>
+
+<!-- sec1 -->
+<section id="sec1">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-5 col-12">
+            <img src="">
+            </div>
+            <div class="col-sm-7 col-12">
+            <h2></h2>
+            <p></p>
+            </div>
+        </div>
+    </div>
+
+<?php include('includes/footer.php'); ?>
