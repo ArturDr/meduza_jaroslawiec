8fa63e4c658aae84945f37c825c57863787f7948
Artur Robek <robek.artur@gmial.com>
2020-07-03 10:51:03 +0200

    add slide to slider

--- medjar/includes/navbar.php
+++ medjar/includes/navbar.php
@@ -27,9 +27,16 @@
                 <li data-target="#carouselGallery" data-slide-to="0" class="active"></li>
                 <li data-target="#carouselGallery" data-slide-to="1"></li>
                 <li data-target="#carouselGallery" data-slide-to="2"></li>
+                <li data-target="#carouselGallery" data-slide-to="3"></li>
             </ol>
         <div class="carousel-inner">
             <div class="carousel-item active">
+                <img src="images/slide111.jpeg" class="d-block w-100">
+                <div class="carousel-caption d-none d-md-block">
+                    <h2>NOCLEGI W JAROSŁAWCU</h2>
+                </div>
+            </div>
+            <div class="carousel-item">
                 <img src="images/slide111.jpeg" class="d-block w-100">
                 <div class="carousel-caption d-none d-md-block">
                     <h2>NOCLEGI W JAROSŁAWCU</h2>
