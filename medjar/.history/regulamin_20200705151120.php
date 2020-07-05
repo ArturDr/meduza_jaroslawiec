ac68273a731dbc4e6e868b10be69aa0634d13962
Artur Robek <robek.artur@gmial.com>
2020-07-05 15:27:08 +0200

    add regulamin subpage & change styles

--- medjar/.history/regulamin_20200705151120.php
+++ medjar/.history/regulamin_20200705151120.php
@@ -41,7 +41,29 @@
     <section id="nsec3">
         <div class="container">
             <div class="row">
-                <p>Prosimy o zapoznanie się z <a href="regulamin.php">regulaminem</a>!</p>
+                <p>Prosimy o zapoznanie się z regulaminem:</p>
+            </div>
+            <div class="row">
+                <ul>
+					<li>Doba hotelowa trwa od godziny 15:00 do godziny 11:00 dnia następnego. </li>
+					<li>Pensjonat ma obowiązek meldunkowy każdego gościa. Uprasza się Gości o okazywanie dowodu stwierdzającego tożsamość. Klient wyraża zgodę na wykorzystanie jego danych osobowych w celu umożliwienia przeprowadzenia pełnego procesu rezerwacji oraz w celu meldunkowym. Dane osobowe nie będą wykorzystywane w innym celu ani również nie będą udostępniane innym podmiotom zgodnie z przepisami ustawy z dnia 28.09.1997r o Ochronie Danych Osobowych.</li>
+					<li>Opłata za pobyt w pensjonacie i opłata klimatyczna pobierana jest w dniu przyjazdu.	W przypadku rezygnacji w trakcie trwania pobytu, pensjonat nie zwraca opłaty za pobyt. </li>
+					<li>Goście wypoczywający w naszym pensjonacie ze zwierzęciem zobowiązani są do zachowania bezpieczeństwa i czystości oraz posiadania aktualnego szczepienia psa. Pies powinien mieć kaganiec i być prowadzony na smyczy. </li>
+					<li>Cisza nocna obowiązuje od godziny 22:00 do godziny 7:00. Osoby odwiedzające naszych Gości proszone są o opuszczenie pokoi pensjonatu przed godziną 22:00.
+					<li>Nie należy przenosić wyposażenia pokoi bez powiadomienia o tym personelu. </li>
+					<li>Wymiana ręczników i sprzątanie pokoju odbywa się na życzenie Gości po dodatkowej opłacie. </li>
+					<li>Pensjonat nie ponosi odpowiedzialności za uszkodzenie lub utratę samochodu lub innego pojazdu należącego do Gościa </li>
+					<li>PARKING JEST NIESTRZEŻONY. </li>
+					<li>Gość ponosi odpowiedzialność materialną za wszelkiego rodzaju uszkodzenia lub zniszczenia przedmiotów wyposażenia lub urządzeń technicznych pensjonatu powstałe z jego winy lub winy odwiedzających go osób. </li>
+					<li>Wszelkie awarie proszę zgłaszać personelowi Pensjonatu w dzień przyjazdu. </li>
+					<li>Ze względu na bezpieczeństwo przeciwpożarowe nie zezwala się na używanie w pokojach pensjonatu grzejników, żelazek i innych podobnych urządzeń, niestanowiących wyposażenia pokoju. Nie dotyczy to ładowarek i zasilaczy urządzeń RTV oraz komputerowych. </li>
+					<li>W obiekcie obowiązuje całkowity zakaz palenia tytoniu. </li>
+					<li>Sieć internetowa bezprzewodowa dostępna jest bezpłatnie kod dostępu u Gospodarza obiektu. </li>
+					<li>Gospodarz wydaje tylko jeden klucz od pokoju. Po zakończeniu pobytu klucz zwrócić Gospodarzowi. Za zgubienie klucza pobierana jest opłata 20 zł. </li>
+				</ul>
+            </div>
+            <div class="row">
+                <p>Zapraszamy serdecznie!</p>
             </div>
         </div>
     </section>
